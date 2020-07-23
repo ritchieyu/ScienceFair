45ee3a053603c0076747b8ad97b138a386a64263
ritchieyu <67218704+ritchieyu@users.noreply.github.com>
2020-07-22 18:11:19 -0600

    Update peak2peak.m

--- peak2peak.m
+++ peak2peak.m
@@ -32,7 +32,7 @@ for p = 1:size(mouseNames,1)
         theFiles = dir(filePattern);
         sorted_files = char(sort_nat({theFiles.name}));
         
-        % Perform analysis on files within each directory
+        % Perform analysis on files within each directory by using algorithm for determining peak-to-peak amplitude
         for i = 1 : size(sorted_files,1)
             baseFileName = deblank(sorted_files(i,:));
             fullFileName = fullfile(directoryNames(k,:), baseFileName);
